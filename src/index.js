6e1cd61952a59dbd963af081ff4691756a09fdc4
AirGamming <41245831+AirGamming@users.noreply.github.com>
2023-02-15 13:27:03 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import style from '/src/css/main.css';
+import * as main from '/src/pages/main.js';
---
+++ src/pages/components/nav.js
@@ -0,0 +1,18 @@
+let navItems = ["today","this week", "projects", "settings"];
+
+
+export default function Nav() {
+    let nav = document.createElement("nav");
+    nav.classList.add("nav");
+    let logo = document.createElement("div");
+    logo.classList.add("logo");
+    logo.innerHTML = `<h1><i class="fa-solid fa-rectangle-list"></i> TODO</h1>`;
+    nav.appendChild(logo);
+    navItems.forEach((item) => {
+        let navItem = document.createElement("button");
+        navItem.classList.add("nav-item");
+        navItem.innerHTML = item;
+        nav.appendChild(navItem);
+    });
+    document.body.appendChild(nav);
+    }
