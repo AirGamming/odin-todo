d677476bc938296bbeaca821a6defc7b394ae573
AirGamming <41245831+AirGamming@users.noreply.github.com>
2023-02-20 23:02:05 +0100

    added popup TOFIX

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import style from '/src/css/main.css';
 import Main from '/src/pages/main.js';
+// import * as favicon from '/favicon.ico';
 
 Main();
+
+const reset = document.createElement("button");
+reset.classList.add("reset");
+reset.innerHTML = "Reset"
+document.body.appendChild(reset);
+
+reset.addEventListener("click", () => {
+    localStorage.clear();
+}
+);
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,33 +1,52 @@
 import Nav from "./components/nav";
 import MenuBar from "./components/menuBar";
+import popupAdd from "./components/popupAdd";
 
 
-let todolist = [
-    {
-        title: "Today",
-        content: "lorem ipsum dolor sit amet",
-        dueDate: "2021-09-01",
-        done: true,
-    },
-    {
-        title: "This week",
-        content: "lorem ipsum dolor sit amet",
-        dueDate: "2021-09-01",
-        done: false,
-    }
-]
+//
+//Paceholder for todolist
+//
+
 export default function Main() {
     Nav();
     MenuBar();
+    let todoitems = [];
+    if(document.cookie){
+        todoitems = document.cookie.split(";").map((todo) => {
+            return JSON.parse(todo.split("=")[1]);
+        }
+        );
+    }
     let content = document.createElement("main");
     document.body.appendChild(content);
     let todoList = document.createElement("div");
     todoList.classList.add("todo-list");
     content.appendChild(todoList);
-    todolist.forEach((todo) => {
+    let headers = document.createElement("div");
+    headers.classList.add("headers");
+
+    let headerTitle = document.createElement("h3");
+    headerTitle.classList.add("header-text");
+    headerTitle.textContent = "done?";
+    let headerContent = document.createElement("h3");
+    headerContent.classList.add("header-text");
+    headerContent.textContent = "Title & Description";
+    let headerDate = document.createElement("h3");
+    headerDate.classList.add("header-text");
+    headerDate.textContent = "Due Date";
+    let headerRemove = document.createElement("h3");
+    headerRemove.classList.add("header-text");
+    headerRemove.textContent = "Remove";
+    headers.append(headerTitle, headerContent, headerDate, headerRemove);
+    todoList.appendChild(headers);
+    todoitems.forEach((todo) => {
         let todoItem = document.createElement("div");
         todoItem.classList.add("todo-item");
         todoList.appendChild(todoItem);
+
+
+        let todoText = document.createElement("div");
+        todoText.classList.add("todo-text");
 
         let todoTitle = document.createElement("h3");
         todoTitle.classList.add("todo-title");
@@ -50,13 +69,19 @@ export default function Main() {
         }else{
             todoDone.removeAttribute("checked");
         }
-
-        todoItem.append(todoTitle, todoContent, todoDueDate, todoDone);
+        let todoRemove = document.createElement("button");
+        todoRemove.classList.add("todo-remove");
+        todoRemove.innerHTML = `<i class="fa-solid fa-x"></i>`;
+        todoRemove.addEventListener("click", () => {
+            todoItem.remove();
+        });
+        todoText.append(todoTitle, todoContent);
+        todoItem.append(todoText, todoDueDate, todoDone, todoRemove);
     }
     )
     let button = document.createElement("button");
     button.classList.add("add-todo");
     button.textContent = "+ Add Todo";
     todoList.appendChild(button);
-
+    button.addEventListener("click", popupAdd);
 }
